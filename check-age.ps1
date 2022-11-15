25237977b0c35682cf4a5780dc74ea12dc2bf89a
floalt <flo.alt@fa-netz.de>
2022-11-15 22:57:30 +0100

    new file added: check-age.ps1

---
+++ check-age.ps1
@@ -0,0 +1,28 @@
+﻿<#
+description
+
+    Checks if $file exists
+    and if is older than $days
+
+author: flo.alt@fa-netz.de
+
+#>
+
+param (
+    $file,
+    $days
+)
+
+if (!(Test-Path $file)) {
+    Write-Host "File does not exist"
+    exit 1001
+}
+
+if (Test-Path $file -OlderThan (Get-Date).AddDays(-$days)) {
+    Write-Host File is older than $days days
+    Write-Host Last Write Time: $(Get-ChildItem $file).LastWriteTime
+    exit 1001
+} else {
+    Write-Host Last Write Time: $(Get-ChildItem $file).LastWriteTime
+    exit 0
+}
